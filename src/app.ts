4abbf89e8234c7b420b70a61ebebd022c3049dfa
RistoFlink <kenssi@gmail.com>
2023-11-09 22:31:35 +0200

    Add more input elements and a handler function

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
@@ -9,11 +12,29 @@ class ProjectInput {
 
 		const importedHTMLContent = document.importNode(this.templateElement.content, true);
 		this.element = importedHTMLContent.firstElementChild as HTMLFormElement;
+		this.element.id = "user-input";
+
+		this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement; 
+		this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+		this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+
+
+		this.configure();
 		this.attach();
 	}
 
 	private attach() {
 		this.hostElement.insertAdjacentElement("afterbegin",this.element);
+	}
+
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler.bind(this));
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		console.log(this.titleInputElement.value);
+
 	}
 }
 
